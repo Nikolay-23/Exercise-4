324479adb30e297445c84e32404fc7bdaecb83c3
4KT <4KT@DESKTOP-J0KOTS8>
2024-11-11 02:31:47 +0000

    Add project files.

---
+++ Exercise-4/Exercise-4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    int number;
+
+    
+    do {
+        printf("Please enter number that's between [1 ... 56]: ");
+        scanf_s("%d", &number);
+
+        if (number < 1 || number > 56) {
+            printf("The number that you enter is false, please try again\n");
+        }
+    } while (number < 1 || number > 56);
+
+    printf("The number you enter is : %d", number);
+}
